c7a08320813d37c8b3aa816df1bfb1ce9ab57ca5
SerhatKalkan <serhatkalkan0127@gmail.com>
2025-03-11 11:19:28 +0100

    Create basic_data_structures.py

---
+++ basic_data_structures.py
@@ -0,0 +1,8 @@
+my_age = 19 # integer 
+my_height = 1.80 # floatiıng point
+my_name = "Serhat" # string
+numbers = [1,2,3] # list
+person = { "Name": "Serhat", "Age": "19"} # dictionary
+colors = ("Red","White") # tuple
+unique_numbers = {1,2,3,4} # set
+is_student = True # true or false
