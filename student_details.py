b7c9d72a4f9022cdc18226f7d177d8a6722bfba8
SerhatKalkan <serhatkalkan0127@gmail.com>
2025-03-13 14:34:38 +0100

    Update and rename main.py to student_details.py

---
+++ student_details.py
@@ -0,0 +1,8 @@
+first_name = "Serhat"
+last_name = "Kalkan"
+index_number = "35522"
+nationality = "Turkish"
+starting_date = "2025-03-01"
+courses = ["Object Oriented Programming","Basics of Telecomunication","Mathematics"]
+
+
