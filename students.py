8810acad5e78d08a937a08e14113510f8a23e719
SerhatKalkan <serhatkalkan0127@gmail.com>
2025-03-13 15:06:56 +0100

    Update students.py

--- students.py
+++ students.py
@@ -6,4 +6,25 @@ students = [
       "nationality": "Turkish",
       "starting_date": "2025-03-01",
       "courses" : [Object Oriented Programming","Basics of Telecomunication","Mathematics"]
-  },
+  },{
+      "first_name": "Berk",
+      "last_name": "Saritas",
+      "index_number": "35236",
+      "nationality": "Turkish",
+      "starting_date": "2025-03-01",
+      "courses" : [Object Oriented Programming","Basics of Telecomunication","Mathematics"]
+  },{
+      "first_name": "Yagiz Berkin",
+      "last_name": "Tosun",
+      "index_number": "35560",
+      "nationality": "Turkish",
+      "starting_date": "2025-03-01",
+      "courses" : [Object Oriented Programming","Basics of Telecomunication","Mathematics"]
+  }
+]                   
+for student in students:
+         print(f"Name:
+         {student['first_name']}
+         {student['last_name']}, Index:
+         {student['index_number']}")
+         
